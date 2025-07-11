56820d5c0c841e2f4a8a6cd9c999e44cb8f8ca91
Muhammad Ibrahim <ranaibrahim453@gmail.com>
2025-07-11 21:57:53 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import ghPages from 'vite-plugin-gh-pages'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    ghPages(),
     react()],
+    base: "/expense-tracker-app/",
 })
